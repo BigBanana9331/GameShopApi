35f2e4b8f34afd08a9f20d62bd973e2b61752347
Huy <phantom39z1@gmail.com>
2023-01-13 10:12:41 +0700

    add order service

---
+++ GameShop.Order/Controllers/OrdersController.cs
@@ -0,0 +1,8 @@
+namespace GameShop.Order.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase{
+        
+    }
+}
---
+++ GameShop.Order/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers(options => { options.SuppressAsyncSuffixInActionNames = false; });
+builder.Services.AddMongo()
+            .AddMongoRepository<CartItem>("carts")
+            .AddMongoRepository<CatalogItem>("catalogs")
+            .AddMongoRepository<Customer>("customers")
+            .AddMassTransitWithRabbitMq();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
